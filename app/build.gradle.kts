34496c73d6c40a3c22468e0c1b16d109327fad0f
Ekaterina <fedorovaea18@yandex.ru>
2023-10-31 23:01:47 +0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,12 +15,16 @@ tasks.compileJava {
 }
 
 application {
-    mainClass = "hexlet.code.App"
+    mainClass.set("hexlet.code.App")
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
 }
 
 tasks.test {
